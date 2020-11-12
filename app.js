5645d62a6d6d140223d182cc079ec1f9d1df2acb
PabloBerho <pablo_fernandez_12@hotmail.com>
2020-11-12 10:58:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express   = require('express')
+const mongoose  = require('mongoose')
+const chalk     = require ('chalk')
+const dotenv    = require ('dotenv')
+const hbs       = require ('hbs')
+const app       = express()
+
+require('dotenv').config()
